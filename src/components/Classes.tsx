b7a9a623b5c735c1b3ac610d82e3f7248993f857
Tanushree Malakar <tanushreemalakar97@gmail.com>
2023-06-10 20:28:34 +0530

    updated logic

--- src/components/Classes.tsx
+++ src/components/Classes.tsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from "react";
 import { CardContainer } from "./CardContainer";
 import { Constants } from "../Utils/Constants";
 import axios from "axios";
+import { ErrorMsg } from "./ErrorMsg";
 
 export const Classes = (props) => {
   const [classList, setClassList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [errorCode, setErrorCode] = useState();
   useEffect(() => {
     let config = {
       method: "get",
@@ -19,12 +21,16 @@ export const Classes = (props) => {
         setClassList(response.data.data);
       })
       .catch((error) => {
+        setIsLoading(false);
+        setErrorCode(error.response.status);
       });
   }, []);
   return (
     <>
       {isLoading ? (
         <h1>Loading ... </h1>
+      ) : errorCode ? (
+        <ErrorMsg errorCode={errorCode} />
       ) : classList.length === 0 ? (
         <h3>No Classes Found</h3>
       ) : (
